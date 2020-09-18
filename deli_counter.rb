45ed3faf6b1b62ecdc6da2ec5fc81137834b62a4
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 01:36:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 def line(katz_deli)
   if katz_deli = []
     puts "The line is currently empty."
-  elsif puts katz_deli  
   end
 end
+
+def line(other_deli)
+  other_deli = ["Logan", "Avi", "Spencer"]
+  puts other_deli
+end
+end
+  

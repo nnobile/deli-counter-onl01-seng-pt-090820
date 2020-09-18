95ea3755e366ef1fb2d4efa9311db438eece02eb
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 01:45:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,24 @@
-def line(katz_deli)
-  if katz_deli = []
+def line(names)
+  if names.empty?
     puts "The line is currently empty."
+  else
+    positions = names.map.with_index(1) {|name, index| "#{index}, #{name}"}
+    stringified_positions = positions.join(" ")
+    puts "The line is currently: " + stringified_positions
   end
 end
 
-def line(other_deli)
-  other_deli = ["Logan", "Avi", "Spencer"]
-  puts other_deli
+def take_a_number(names, name_to_add)
+  names.push(name_to_add)
+  puts "Welcome, #{name_to_add}. You are number #{names.length} in line."
 end
+
+def now_serving(names)
+  if names.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{names.first}"
+    names.shift
+  end
+end
+  
